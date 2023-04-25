a9255c22a78c6bff36f66921eeef48fb7292d8e0
Kataplan <dcatalanmarchese@gmail.com>
2023-04-25 19:06:19 -0400

    Update routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './views/home/home.component';
+import { GrassComponent } from './views/grass/grass.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', component: HomeComponent},
+  {path:'grass', component: GrassComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
