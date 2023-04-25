90bf6ee43e5dbdc5494c6cdd72c517b59c359b04
Kataplan <dcatalanmarchese@gmail.com>
2023-04-25 19:04:55 -0400

    create pokeball selector component

---
+++ src/app/components/pokeball-selector-component/pokeball-selector-component.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pokeball-selector-component',
+  templateUrl: './pokeball-selector-component.component.html',
+  styleUrls: ['./pokeball-selector-component.component.scss']
+})
+export class PokeballSelectorComponentComponent {
+
+}
