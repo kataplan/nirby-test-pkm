5d6b9578ebda529eb30093798dde4d770d8ed935
Kataplan <dcatalanmarchese@gmail.com>
2023-04-25 18:31:38 -0400

    init

---
+++ src/environments/environment.ts
@@ -0,0 +1,9 @@
+export const environment = {
+  firebase: {
+    projectId: 'pokemon-nirby',
+    appId: '1:1043997279966:web:6efdcc3f1a9c5071ffa44f',
+    storageBucket: 'pokemon-nirby.appspot.com',
+    apiKey: 'AIzaSyCricdmTHCoi_tXXBUda4UCdHAcyj_pT7E',
+    authDomain: 'pokemon-nirby.firebaseapp.com',
+    messagingSenderId: '1043997279966',
+  },};
