b08bf4c4319fa25636421e9c2305fca657ab7d2d
Kataplan <dcatalanmarchese@gmail.com>
2023-04-25 19:05:09 -0400

    create pokemon view component

---
+++ src/app/components/pokemon-view-component/pokemon-view-component.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pokemon-view-component',
+  templateUrl: './pokemon-view-component.component.html',
+  styleUrls: ['./pokemon-view-component.component.scss']
+})
+export class PokemonViewComponentComponent {
+
+}
