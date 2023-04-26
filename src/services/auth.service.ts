7d4d32ec670bc2ec2df70375e82800e49d7b74fe
Kataplan <dcatalanmarchese@gmail.com>
2023-04-26 16:07:08 -0400

    Add Auth services

---
+++ src/services/auth.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import { Auth,createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut,GoogleAuthProvider } from '@angular/fire/auth';
+import { signInWithPopup } from 'firebase/auth';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  constructor(private auth:Auth) { }
+  register({email,password}:any){
+    return createUserWithEmailAndPassword(this.auth,email,password)
+  }
+
+  login({email,password}:any){
+    return signInWithEmailAndPassword(this.auth,email,password)
+  }
+  loginWithGoogle(){
+    return signInWithPopup(this.auth, new GoogleAuthProvider)
+  }
+  logout(){
+    return signOut(this.auth)
+  }
+  
+}
