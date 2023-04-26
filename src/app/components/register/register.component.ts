24b984190e251cc690862325ec3ec9d60376ed86
Kataplan <dcatalanmarchese@gmail.com>
2023-04-26 16:06:14 -0400

    Add register component

---
+++ src/app/components/register/register.component.ts
@@ -0,0 +1,39 @@
+import { Component } from '@angular/core';
+import { AuthService } from 'src/services/auth.service';
+import { FormBuilder, FormControl } from '@angular/forms';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.scss']
+})
+export class RegisterComponent {
+
+  registerForm = this.formBuilder.group({
+    email: new FormControl(),
+    password: new FormControl()
+  });
+  
+  constructor(
+    private authServices: AuthService,
+    private formBuilder: FormBuilder,
+    private router: Router
+
+  ) { }
+
+  onRegisterSubmit(): void {
+    this.authServices.register(this.registerForm.value)
+      .then(() =>  
+        this.router.navigate(['']))
+      .catch(error => console.log(error))
+    
+    this.registerForm.reset();
+  }
+  onGoogleSignIn():void{
+    this.authServices.loginWithGoogle()
+    .then(() => {
+      this.router.navigate(['/grass'])
+    })
+  }
+}
