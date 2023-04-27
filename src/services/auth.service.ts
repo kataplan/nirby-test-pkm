880c411591d031b9885dc2b6d1e0e2fc61b5f401
Kataplan <dcatalanmarchese@gmail.com>
2023-04-27 18:49:26 -0400

    Update auth service

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -1,25 +1,37 @@
-import { Injectable } from '@angular/core';
-import { Auth,createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut,GoogleAuthProvider } from '@angular/fire/auth';
-import { signInWithPopup } from 'firebase/auth';
+import { Injectable, inject } from '@angular/core';
+import { Auth, createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut, GoogleAuthProvider, authState } from '@angular/fire/auth';
+import { User, signInWithPopup } from 'firebase/auth';
+import { Observable, Subscription } from 'rxjs';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
+  authState = authState(this.auth);
+  constructor(
+    private auth: Auth,
+  ) {}
+  
 
-  constructor(private auth:Auth) { }
-  register({email,password}:any){
-    return createUserWithEmailAndPassword(this.auth,email,password)
+  register({ email, password }: any) {
+    return createUserWithEmailAndPassword(this.auth, email, password)
   }
 
-  login({email,password}:any){
-    return signInWithEmailAndPassword(this.auth,email,password)
+  login({ email, password }: any) {
+    return signInWithEmailAndPassword(this.auth, email, password)
   }
-  loginWithGoogle(){
+
+  loginWithGoogle() {
     return signInWithPopup(this.auth, new GoogleAuthProvider)
   }
-  logout(){
+
+  logout() {
     return signOut(this.auth)
   }
-  
+
+  getUserStatus(){
+    return this.authState
+  }
+
 }
