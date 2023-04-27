99b25464236136f57181513763740c50ee7142f4
Kataplan <dcatalanmarchese@gmail.com>
2023-04-27 18:49:18 -0400

    Update pokemon service

--- src/services/pokemon.service.ts
+++ src/services/pokemon.service.ts
@@ -1,36 +1,36 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http'
 import { Limit } from 'src/interfaces/limit';
-import { Observable, map } from 'rxjs'
-import { Pokemon } from 'src/interfaces/pokemon';
+import { Observable} from 'rxjs'
+import { IPokemonRaw, Pokemon } from 'src/interfaces/pokemon';
 @Injectable({
   providedIn: 'root'
 })
 export class PokemonService {
   private URL = "https://pokeapi.co/api/v2/"
-  maxId:number = 0
+  maxId: number = 0
   currentPokemon: Pokemon = {
-    id: 0,
-    types: [""],
-    name: "",
-    sprites:{front_default:""}
+    pokemonId: 0,
+    pokemonType:[""],
+    pokemonName: "",
+    pokemonImageUrl:"",
 
   };
   constructor(private http: HttpClient) { }
 
-  public setMaxId(id:number):void{
-    this.maxId=id
+  public setMaxId(id: number): void {
+    this.maxId = id
   }
-  public setCurrentPokemon(pokemon:Pokemon):void{
-    this.currentPokemon=pokemon
+  public setCurrentPokemon(pokemon: Pokemon): void {
+    this.currentPokemon = pokemon
+    }
+  public getPokemon(index: number): Observable<IPokemonRaw> {
+    return this.http.get<IPokemonRaw>(`${this.URL}pokemon/${index}`)
   }
-  public getPokemon(index:number):Observable<Pokemon>{
-    return this.http.get<Pokemon>(`${this.URL}pokemon/${index}`)
+  public getPokemonByName(name: string): Observable<IPokemonRaw> {
+    return this.http.get<IPokemonRaw>(`${this.URL}pokemon/${name}`)
   }
-  public getPokemonByName(name:string):Observable<Pokemon>{
-    return this.http.get<Pokemon>(`${this.URL}pokemon/${name}`)
-  }
-  public getPokemonLimit():Observable<Limit>{
+  public getPokemonLimit(): Observable<Limit> {
     return this.http.get<Limit>(`${this.URL}pokemon-species?limit=100000&offset=0)`)
   }
 }
