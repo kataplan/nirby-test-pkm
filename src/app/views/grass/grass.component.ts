9449f1f6841fd2cead57391e31e754ef2cb7a6f2
Kataplan <dcatalanmarchese@gmail.com>
2023-04-27 18:47:30 -0400

    Add grass component style, function, structure

--- src/app/views/grass/grass.component.ts
+++ src/app/views/grass/grass.component.ts
@@ -1,4 +1,9 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { Limit } from 'src/interfaces/limit';
+import { IPokemonRaw, IPokemonResult, Pokemon } from 'src/interfaces/pokemon';
+import { AuthService } from 'src/services/auth.service';
+import { PokemonService } from 'src/services/pokemon.service';
 
 @Component({
   selector: 'app-grass',
@@ -6,5 +11,57 @@ import { Component } from '@angular/core';
   styleUrls: ['./grass.component.scss']
 })
 export class GrassComponent {
+  maxId: number = 0;
+
+  constructor(
+    private authServices: AuthService,
+    private router: Router,
+    public pokemonServices: PokemonService,
+  ) { }
+
+
+  ngOnInit() {
+    this.getFirstPokemon()
+  }
+
+  logout() {
+    this.authServices.logout()
+      .then(() => {
+        this.router.navigate(['/'])
+      })
+  }
+  navigate(str: String) {
+    this.router.navigate([str])
+  }
+  getFirstPokemon(): void {
+    this.pokemonServices.getPokemonLimit().subscribe(
+      (res: Limit) => {
+        const lastPokemon: IPokemonResult = res.results[res.results.length - 1]
+        console.log(lastPokemon);
+        this.pokemonServices.getPokemonByName(lastPokemon.name).subscribe(
+          (res: IPokemonRaw) => {
+            console.log(res);
+            this.maxId = res.id;
+            this.pokemonServices.setMaxId(this.maxId)
+            const id = Math.round(this.maxId * Math.random())
+            this.pokemonServices.getPokemon(id).subscribe(
+              (res: IPokemonRaw) => {
+                console.log(res.name);
+
+                const newPokemon: Pokemon = {
+                  pokemonId: res.id,
+                  pokemonName: res.name,
+                  pokemonType: res.types.map(({ type }) => type.name),
+                  pokemonImageUrl: res.sprites.front_default
+
+                }
+                this.pokemonServices.setCurrentPokemon(newPokemon)
+              }
+            );
+          }
+        )
+      }
+    )
+  }
 
 }
