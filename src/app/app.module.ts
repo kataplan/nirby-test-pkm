cadde62e54248f737a6efd2b8289a14df438563c
Kataplan <dcatalanmarchese@gmail.com>
2023-04-27 18:45:07 -0400

    Add imports and reorganize

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,23 +3,30 @@ import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientModule } from '@angular/common/http';
 import { ReactiveFormsModule} from '@angular/forms'
-import { AppRoutingModule } from './app-routing.module';
-import { DividerModule } from 'primeng/divider';
-import { AppComponent } from './app.component';
+import { DatePipe, TitleCasePipe } from '@angular/common';
 import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
-import { environment } from '../environments/environment';
-import { provideAuth,getAuth } from '@angular/fire/auth';
 import { provideFirestore,getFirestore } from '@angular/fire/firestore';
-import { PokemonViewComponentComponent } from './components/pokemon-view-component/pokemon-view-component.component';
-import { PokeballSelectorComponentComponent } from './components/pokeball-selector-component/pokeball-selector-component.component';
-import { HomeComponent } from './views/home/home.component';
-import { GrassComponent } from './views/grass/grass.component';
-import { LoginComponent } from './components/login/login.component';
-import { RegisterComponent } from './components/register/register.component';
+import { provideAuth,getAuth } from '@angular/fire/auth';
+import { AppComponent } from './app.component';
+import { environment } from '../environments/environment';
+import { AppRoutingModule } from './app-routing.module';
+
+//PrimeNg imports
+import { DividerModule } from 'primeng/divider';
 import { InputTextModule } from 'primeng/inputtext';
 import { ButtonModule } from 'primeng/button';
 import { MenuModule } from 'primeng/menu';
 import { ToastModule } from 'primeng/toast';
+import { CardModule } from 'primeng/card';
+
+// Componets
+import { PokemonViewComponentComponent } from './components/pokemon-view-component/pokemon-view-component.component';
+import { PokeballSelectorComponentComponent } from './components/pokeball-selector-component/pokeball-selector-component.component';
+import { HomeComponent } from './views/home/home.component';
+import { GrassComponent } from './views/grass/grass.component';
+import { CapturesComponent } from './views/captures/captures.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
 
 @NgModule({
   declarations: [
@@ -29,7 +36,9 @@ import { ToastModule } from 'primeng/toast';
     HomeComponent,
     GrassComponent,
     LoginComponent,
-    RegisterComponent
+    RegisterComponent,
+    CapturesComponent,
+    
   ],
   imports: [
     BrowserModule,
@@ -42,6 +51,11 @@ import { ToastModule } from 'primeng/toast';
     AppRoutingModule,
     MenuModule,
     ToastModule,
+    CardModule,
+    
+    DatePipe,
+    TitleCasePipe,
+    
     provideFirebaseApp(() => initializeApp(environment.firebase)),
     provideAuth(() => getAuth()),
     provideFirestore(() => getFirestore())
