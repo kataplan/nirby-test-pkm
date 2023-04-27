c9e50db964bea9a74df199de2298bbdc6619a583
Kataplan <dcatalanmarchese@gmail.com>
2023-04-26 20:56:34 -0400

    Update routing rules

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './views/home/home.component';
 import { GrassComponent } from './views/grass/grass.component';
+import { canActivate, redirectUnauthorizedTo, redirectLoggedInTo } from '@angular/fire/auth-guard';
 
 const routes: Routes = [
-  {path:'', component: HomeComponent},
-  {path:'grass', component: GrassComponent}
+  { path: '', component: HomeComponent, 
+  ...canActivate(() => redirectLoggedInTo(['/grass'])) },
+  { path: 'grass', component: GrassComponent, 
+  ...canActivate(() => redirectUnauthorizedTo([''])) 
+}
 ];
 
 @NgModule({
