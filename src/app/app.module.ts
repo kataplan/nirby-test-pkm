ad0d6a8f7e2988a8fdc7c35b0d5f43d5418a1c75
Kataplan <dcatalanmarchese@gmail.com>
2023-04-26 22:20:24 -0400

    Add modules of primeng and components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
+import { ReactiveFormsModule} from '@angular/forms'
 import { AppRoutingModule } from './app-routing.module';
+import { DividerModule } from 'primeng/divider';
 import { AppComponent } from './app.component';
 import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
 import { environment } from '../environments/environment';
@@ -11,6 +14,12 @@ import { PokemonViewComponentComponent } from './components/pokemon-view-compone
 import { PokeballSelectorComponentComponent } from './components/pokeball-selector-component/pokeball-selector-component.component';
 import { HomeComponent } from './views/home/home.component';
 import { GrassComponent } from './views/grass/grass.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { InputTextModule } from 'primeng/inputtext';
+import { ButtonModule } from 'primeng/button';
+import { MenuModule } from 'primeng/menu';
+import { ToastModule } from 'primeng/toast';
 
 @NgModule({
   declarations: [
@@ -18,11 +27,21 @@ import { GrassComponent } from './views/grass/grass.component';
     PokemonViewComponentComponent,
     PokeballSelectorComponentComponent,
     HomeComponent,
-    GrassComponent
+    GrassComponent,
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    ButtonModule,
+    InputTextModule,
+    ReactiveFormsModule,
+    DividerModule,
     AppRoutingModule,
+    MenuModule,
+    ToastModule,
     provideFirebaseApp(() => initializeApp(environment.firebase)),
     provideAuth(() => getAuth()),
     provideFirestore(() => getFirestore())
