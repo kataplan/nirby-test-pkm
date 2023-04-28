ff6a0d209009ee142d4f881528307f6e9ebe5381
Kataplan <dcatalanmarchese@gmail.com>
2023-04-28 15:31:28 -0400

    Document captures.component.ts

--- src/app/views/captures/captures.component.ts
+++ src/app/views/captures/captures.component.ts
@@ -11,32 +11,41 @@ import { PokemonCaptureService } from 'src/services/pokemonCapture.service';
 })
 export class CapturesComponent {
   captureList: PokemonCapture[] = []
-  
+
   constructor(
     private authServices: AuthService,
     private pokemonCaptureService: PokemonCaptureService,
     private router: Router,
   ) { }
-  
+
   ngOnInit() {
     this.getPokemons()
   }
-
-  navigate(str:String){
+  /**
+   * Function to navigate to a new page
+   * @param str a string with a URL
+   */
+  navigate(str: String) {
     this.router.navigate([str])
   }
-
+  /**
+  * Function to logout and navigate to home page ("/")
+  */
   logout() {
     this.authServices.logout()
       .then(() => {
         this.router.navigate(['/'])
       })
   }
-
-  getPokemons() {
+  
+  /**
+  * Function to get all the userPokemons and trnsform that PokemonCaptureRaw list to a pokemonCapture list.
+  *   
+  */
+  getPokemons(): void {
     this.pokemonCaptureService.getUserPokemon().subscribe(
-      (val: PokemonCaptureRaw[]) => this.captureList= val.map<PokemonCapture>(
-        ({ captureTime, pokemonId, pokemonName, pokemonType, pokemonImageUrl, userId,documentId }) => {
+      (val: PokemonCaptureRaw[]) => this.captureList = val.map<PokemonCapture>(
+        ({ captureTime, pokemonId, pokemonName, pokemonType, pokemonImageUrl, userId, documentId }) => {
           const newPokemon: PokemonCapture = {
             pokemonId: pokemonId,
             pokemonName: pokemonName,
@@ -51,7 +60,12 @@ export class CapturesComponent {
       )
     )
   }
-  liberatePokemon(pokemon:PokemonCapture){
+
+  /**
+  * Function to call a service that remove a user's pokemon from firestore db.
+  * @param pokemon to be removed.
+  */
+  liberatePokemon(pokemon: PokemonCapture): void {
     this.pokemonCaptureService.removePokemon(pokemon)
   }
 }
