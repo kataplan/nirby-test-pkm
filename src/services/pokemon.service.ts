13fb6e803eac2b8933c8b9f321e11974feb1f590
Kataplan <dcatalanmarchese@gmail.com>
2023-04-28 14:58:36 -0400

    Add documentation to pokemon.service

--- src/services/pokemon.service.ts
+++ src/services/pokemon.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http'
-import { Limit } from 'src/interfaces/limit';
-import { Observable} from 'rxjs'
+import { SpeciesList } from 'src/interfaces/speciesList';
+import { Observable } from 'rxjs'
 import { IPokemonRaw, Pokemon } from 'src/interfaces/pokemon';
 @Injectable({
   providedIn: 'root'
@@ -11,26 +11,52 @@ export class PokemonService {
   maxId: number = 0
   currentPokemon: Pokemon = {
     pokemonId: 0,
-    pokemonType:[""],
+    pokemonType: [""],
     pokemonName: "",
-    pokemonImageUrl:"",
+    pokemonImageUrl: "",
 
   };
   constructor(private http: HttpClient) { }
 
+  /**
+  * Setter of maxId.
+  * @param id the new max id
+  */
   public setMaxId(id: number): void {
     this.maxId = id
   }
+
+  /**
+  * Setter of currentPokemon.
+  * @param pokemon the new currentPokemon
+  */
   public setCurrentPokemon(pokemon: Pokemon): void {
     this.currentPokemon = pokemon
-    }
-  public getPokemon(index: number): Observable<IPokemonRaw> {
-    return this.http.get<IPokemonRaw>(`${this.URL}pokemon/${index}`)
   }
+
+  /**
+  * Get a pokemon from a http consult to PokemonAPI by id
+  * @param id for a new pokemon to get
+  * @returns Ipokemon raw object result from http consult
+  */
+  public getPokemon(id: number): Observable<IPokemonRaw> {
+    return this.http.get<IPokemonRaw>(`${this.URL}pokemon/${id}`)
+  }
+
+  /**
+  * Get a pokemon from a http consult to PokemonAPI by name
+  * @param name for a new pokemon to get
+  * @returns Ipokemon raw object result from http consult
+  */
   public getPokemonByName(name: string): Observable<IPokemonRaw> {
     return this.http.get<IPokemonRaw>(`${this.URL}pokemon/${name}`)
   }
-  public getPokemonLimit(): Observable<Limit> {
-    return this.http.get<Limit>(`${this.URL}pokemon-species?limit=100000&offset=0)`)
+
+  /**
+  * Get a List with all the pokemon species
+  * @returns Observable of speciesList result from http consult
+  */
+  public getPokemonSpeciesList(): Observable<SpeciesList> {
+    return this.http.get<SpeciesList>(`${this.URL}pokemon-species?limit=100000&offset=0)`)
   }
 }
